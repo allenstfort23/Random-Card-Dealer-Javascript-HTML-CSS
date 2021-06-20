1a0e4728e75ebf7bae02e1eec1edca2f98f151ce
Allen St Fort <allenstfort1@gmail.com>
2021-06-20 14:03:53 +0000

    Finished Random card

--- src/app.js
+++ src/app.js
@@ -7,4 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let suiteTop = document.querySelector(".top-logo"); // this contains the top sutie
+  let suiteBottom = document.querySelector(".bottom-logo"); // this contains the bottom suite
+  let suiteNumber = document.querySelector(".number"); // this contains the number of the suite
+
+  let generateRandomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[randomNumbers];
+  };
+
+  let generatorRandomSuite = () => {
+    let suite = ["&spades;", "&hearts;", "&diams;", "&clubs;"];
+    let randomSuite = Math.floor(Math.random() * suite.length);
+    return suite[randomSuite];
+  };
+
+  suiteNumber.innerHTML = generateRandomNumber();
+  let same = generatorRandomSuite();
+  let top = (suiteTop.innerHTML = same);
+  let bottom = (suiteBottom.innerHTML = same);
 };
